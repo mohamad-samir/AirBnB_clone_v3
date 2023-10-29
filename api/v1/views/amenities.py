7996813319b4c3a9345d90d15dd5846b2eefa918
mohamad-samir <mohamad.samir2011@gmail.com>
2023-10-29 18:56:04 +0200

    u

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,11 +7,10 @@ from models.amenity import Amenity
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-def get_amenities():
+def amenities():
     """Retrieves the list of all Amenity objects"""
-    amenities = storage.all(Amenity).values()
-    amenities_list = [amenity.to_dict() for amenity in amenities]
-    return jsonify(amenities_list)
+    objs = storage.all(Amenity)
+    return jsonify([obj.to_dict() for obj in objs.values()])
 
 
 @app_views.route('/amenities/<amenity_id>',
